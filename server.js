2706e62a4db8912ab3d7589f908611c715c9244c
Nima <nima97e@gmail.com>
2022-06-06 16:36:29 +0300

    add body-parser

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const bodyParser= require('body-parser')
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
 app.get('/', (req, res)=> {
     res.sendFile(__dirname + '/index.html')
 })
+
+app.post('/quotes', (req, res) => {
+    console.log('Hellooooooooooooooooo!')
+  })
 
 app.listen(3000, ()=> {
     console.log('listening on port 3000')
