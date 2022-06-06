827f355a58dcffe728d44c995690689eb33d05fa
Nima <nima97e@gmail.com>
2022-06-06 16:24:26 +0300

    create index.html

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 
 
+app.get('/', (req, res)=> {
+    res.sendFile(__dirname + '/index.html')
+})
+
 app.listen(3000, ()=> {
     console.log('listening on port 3000')
 })
